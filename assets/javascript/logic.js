56b8e2c375e8386592e8065a480c1d9ebddca01e
Donnie Toombs <donnietoombs@gmail.com>
2018-08-16 15:31:28 -0400

    update logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,19 +4,18 @@
 $("#searchTeamBtn").on("click", function(event) {
   event.preventDefault();
 
-$("#testDiv").empty();
-
+$("#gameDiv").empty();
+$("#dt-tmDiv").empty();
+$("#locationDiv").empty();
 
 // Grabs user input
-var trainName = $("#teamNameInput").val().trim();
-var testName = $("#teamNameInput").val($(this).val().toLowerCase().replace(" ", "-"));
-trainName.toLowerCase();
-var lowStr= trainName.toLowerCase();
+var teamName = $("#teamNameInput").val().trim();
+teamName.toLowerCase();
+var lowStr= teamName.toLowerCase();
 var formattedText =lowStr.replace(/ /g,"-");
 console.log(formattedText);
 
-var teamName = "washington-redskins";
-var queryURL = "https://api.seatgeek.com/2/events?performers.slug=" + formattedText + "&client_id=MTI2OTM3NTd8MTUzNDIxNzA0OS45Mg&client_secret=5a186662f95a2be9b6b3a7e0de031a42b656c00e20c4c969c867cdb40bee6811";
+var queryURL = "https://api.seatgeek.com/2/events?performers.slug=" + formattedText + "&client_id=MTI2OTM3NTd8MTUzNDIxNzA0OS45Mg&client_secret=5a186662f95a2be9b6b3a7e0de031a42b656c00e20c4c969c867cdb40bee6811&per_page=19";
 
 $.ajax({
   url: queryURL,
@@ -25,30 +24,38 @@ $.ajax({
   console.log(response);
   console.log(response.events);
   console.log(response.events[0]);
-  
-
+  $("#informBox").text("Click Link Below to go to SeatGeek to Buy Tix to Game!");
   for (i =0; i<response.events.length; i++ ) {
   var respEvent = response.events[i].short_title;
   console.log(respEvent);
 
-    //   var newDivs = $("<p class='games'>");
-    //    $(".games").text(testShow);
-  
-     $("#testDiv").append("<ul>" + respEvent + "</ul>");
-  }
+    var gameLink = $("<a>");
+    gameLink.attr("href", response.events[i].url);
+    gameLink.attr("title",response.events[i].short_title);
+    gameLink.text(response.events[i].short_title);
+    gameLink.addClass("gameLink");
+    console.log(gameLink);
+     $("#gameDiv").append(gameLink);
 
+     var dttmLink = $("<p>");
+     dttmLink.text(response.events[i].datetime_local);
+    console.log(dttmLink);
+    $("#dt-tmDiv").append(dttmLink);
+
+    var locationLink =$("<p>");
+    locationLink.text(response.events[i].venue.name);
+    console.log(locationLink);
+    $("#locationDiv").append(locationLink);
+  }
 });
 
 var queryURL2 = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + formattedText;
 
-// Perfoming an AJAX GET request to our queryURL
 $.ajax({
   url: queryURL2,
-  method: "GET"
-})
-
-// After the data from the AJAX request comes back
-  .then(function(response) {
+  method: "GET",
+  dataType: 'jsonp'
+}).then(function(response) {
     console.log(response);
 
   // Saving the image_original_url property
@@ -63,9 +70,20 @@ $.ajax({
 
     // Prepending the teamImage to the images div
     $("#teamImage").html(teamImage);
-  });
+});
 
 
+// var queryURL4 =  "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.1.2&SECURITY-APPNAME=DonnieTo-ProFootb-PRD-3262b1842-e568d10e&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=harry%20potter%20phoenix&paginationInput.entriesPerPage=2";
+var queryURL5 ="http://svcs.ebay.com/services/search/FindingService/v1?SERVICE-NAME=FindingService&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.12.0&SECURITY-APPNAME=DonnieTo-ProFootb-PRD-3262b1842-e568d10e&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&GLOBAL-ID=EBAY-US&keywords=cats&paginationInput.entriesPerPage=25&paginationInput.pageNumber=1"
+
+$.ajax({
+  // url: queryURL4,
+  url: queryURL5,
+ method: "GET"
+}).then(function(response) {
+  console.log(response); 
+  console.log(response.findItemsByKeywordsResponse);
+});
 
 
 
@@ -87,7 +105,7 @@ $.ajax({
 
 
 /*var queryURL2="https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=washingtonredskins";
-// "https://api.giphy.com/v1/gifs/search?q=" + trainName + "&api_key=Y5629Z6gX2pYj8IIsreLeaMMm5ZOjUvi";
+// "https://api.giphy.com/v1/gifs/search?q=" + teamName + "&api_key=Y5629Z6gX2pYj8IIsreLeaMMm5ZOjUvi";
 
 $.ajax({
   url: queryURL2,
